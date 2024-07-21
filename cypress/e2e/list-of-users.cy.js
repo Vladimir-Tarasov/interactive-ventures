4dc5ad84acd210625e824cf0dff3497b51156ae6
Vladimir Tarasov <vtarasov.sk@gmail.com>
2024-07-21 17:46:40 +0200

    Test for list of users

--- cypress/e2e/list-of-users.cy.js
+++ cypress/e2e/list-of-users.cy.js
@@ -3,23 +3,95 @@ describe('Testing API of list of users', () => {
         cy.request('Get', 'https://hr-challenge.dev.tapyou.com/api/test/users?gender=male')
             .then(({ status, body }) => {
                 expect(status).to.eq(200);
-                expect(body).to.have.all.keys('isSuccess', 'errorCode', 'errorMessage', 'idList');
-                expect(body.isSuccess).to.be.true;
+                expect(body).to.have.all.keys('success', 'errorCode', 'errorMessage', 'result');
+                expect(body.success).to.be.true;
                 expect(body.errorCode).to.eq(0);
                 expect(body.errorMessage).to.be.a('null');
-                expect(body.idList).to.be.an('array');
+                expect(body.result).to.be.an('array');
+                body.result.forEach(id => {
+                    expect(id).to.be.a('number');
+                });
             })
-    })
+    });
 
     it('should test list of users with gender=female', () => {
         cy.request('Get', 'https://hr-challenge.dev.tapyou.com/api/test/users?gender=female')
             .then(({ status, body }) => {
                 expect(status).to.eq(200);
-                expect(body).to.have.all.keys('isSuccess', 'errorCode', 'errorMessage', 'idList');
-                expect(body.isSuccess).to.be.true;
+                expect(body).to.have.all.keys('success', 'errorCode', 'errorMessage', 'result');
+                expect(body.success).to.be.true;
                 expect(body.errorCode).to.eq(0);
                 expect(body.errorMessage).to.be.a('null');
-                expect(body.idList).to.be.an('array');
+                expect(body.result).to.be.an('array');
+                body.result.forEach(id => {
+                    expect(id).to.be.a('number');
+                });
             })
-    })
+    });
+
+    it('should test list of users with gender=magic', () => {
+        cy.request({
+            method: 'Get',
+            url: 'https://hr-challenge.dev.tapyou.com/api/test/users?gender=magic',
+            failOnStatusCode: false
+        })
+            .then(({ status, body }) => {
+                expect(status).to.eq(400);
+            })
+    });
+
+    it('should test list of users with gender=McCloud', () => {
+        cy.request({
+            method: 'Get',
+            url: 'https://hr-challenge.dev.tapyou.com/api/test/users?gender=McCloud',
+            failOnStatusCode: false
+        })
+            .then(({ status, body }) => {
+                expect(status).to.eq(400);
+            })
+    });
+
+    it('should test list of users with gender=any', () => {
+        cy.request({
+            method: 'Get',
+            url: 'https://hr-challenge.dev.tapyou.com/api/test/users?gender=any',
+            failOnStatusCode: false
+        })
+            .then(({ status, body }) => {
+                expect(status).to.eq(400);
+            })
+    });
+
+    it('should test list of users with gender= ', () => {
+        cy.request({
+            method: 'Get',
+            url: 'https://hr-challenge.dev.tapyou.com/api/test/users?gender= ',
+            failOnStatusCode: false
+        })
+            .then(({ status, body }) => {
+                expect(status).to.eq(400);
+            })
+    });
+
+    it('should test list of users with gender=!!&&%%$$', () => {
+        cy.request({
+            method: 'Get',
+            url: 'https://hr-challenge.dev.tapyou.com/api/test/users?gender=!!&&%%$$',
+            failOnStatusCode: false
+        })
+            .then(({ status, body }) => {
+                expect(status).to.eq(400);
+            })
+    });
+
+    it('should test list of users with gender=MALE', () => {
+        cy.request({
+            method: 'Get',
+            url: 'https://hr-challenge.dev.tapyou.com/api/test/users?gender=MALE',
+            failOnStatusCode: false
+        })
+            .then(({ status, body }) => {
+                expect(status).to.eq(400);
+            })
+    });
 })
